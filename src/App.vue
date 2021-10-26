87b7026e8a33368b8deeb659d73c739aa2380c73
Den <zgunnar@yandex.ru>
2021-10-26 18:53:47 +0300

    vue 3 lesson

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,128 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div :class="[$style.wrapper]">
+      <header>
+        <div :class="[$style.title]">My personal costs</div>
+        <div :class="[$style.title_content]">
+          <div :class="[$style.id_number]">#</div>
+          <div :class="[$style.date]">Date</div>
+          <div :class="[$style.category]">Category</div>
+          <div :class="[$style.value]">Value</div>
+        </div>
+      </header>
+
+      <main>
+        <PaymentsDisplay :items="paymentsList" />
+        <!-- <PaymentsDisplay :items="'any list data'" />-->
+      </main>
+
+      <CounterButton />
+      <!--
+      <button v-if="showKey">
+        <PaymentForm @addNewPayment="addNewPayment" />
+          
+        
+      </button>-->
+      <PaymentForm @addNewPayment="addNewPayment" />
     </div>
-    <router-view/>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import CounterButton from "@/components/counterButton.vue";
+import PaymentsDisplay from "@/components/paymentDisplay.vue";
+import PaymentForm from "@/components/addPaymenForm.vue";
+//import AddPaymentForm from './components/AddPaymentForm
 
-<style lang="scss">
+export default {
+  //name: 'Home',
+  components: {
+    CounterButton,
+    PaymentsDisplay,
+    PaymentForm,
+  },
+
+  data() {
+    return {
+      paymentsList: [],
+      showKey: false,
+    };
+  },
+  methods: {
+    addNewPayment(data) {
+      data.number = data.number + this.paymentsList.length;
+      this.paymentsList = [...this.paymentsList, data];
+    },
+
+    fetchData() {
+      return [
+        {
+          number: ++this.paymentsList.length,
+          date: "28.03.2020",
+          category: "Food",
+          value: 169,
+        },
+        {
+          number: ++this.paymentsList.length,
+          date: "24.03.2020",
+          category: "Transport",
+          value: 360,
+        },
+        {
+          number: ++this.paymentsList.length,
+          date: "24.03.2020",
+          category: "Food",
+          value: 532,
+        },
+      ];
+    },
+  },
+  created() {
+    this.paymentsList = this.fetchData();
+  },
+};
+</script>
+<style lang="scss" module>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.wrapper {
+  background-color: aquamarine;
+  border: 1px solid black;
+  width: 700px;
+}
+
+.title {
+  text-align: center;
+  //border: 1px solid black;
+  font-size: 28px;
+}
+.title_content {
+  border: 1px solid black;
+  display: flex;
+  box-sizing: border-box;
+  text-align: center;
+}
+.date {
+  border: 1px solid black;
+  width: 150px;
+}
+.id_number {
+  border: 1px solid black;
+  width: 50px;
+}
+.category {
+  border: 1px solid black;
+  width: 400px;
+}
+.value {
+  border: 1px solid black;
+  width: 100px;
 }
 
 #nav {
---
+++ src/components/counterButton.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+ <!--<button>Hover!</button>
+-->
+  <button @click="onClick">Clicked {{ counter }} times!</button>
+  </div>
+</template>
+ 
+<script>
+  export default {
+    
+  name: 'CounterButton',
+  
+
+    data () {
+      return {
+        counter: 0,
+      }
+    },
+    methods: {
+      onClick () {
+        this.counter++
+       },
+       onMouseOver () {
+        console.log('Hovered on button!')
+      }
+
+
+    },
+    beforeMount () {
+        console.log('Component root element: ', this.$el)
+    },
+    mounted () {
+      const btn = this.$el.querySelector('button')
+      if (btn) {
+        btn.addEventListener('mouseover', this.onMouseOver)
+      }
+    },
+    beforeDestroy () {
+  const btn = this.$el.querySelector('button')
+  if (btn) {
+    btn.removeEventListener('onmouseover', this.onHover)
+  }
+},
+
+
+
+  }
+</script>
